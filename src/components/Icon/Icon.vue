0186a98de9a5ac5c559c273f6a3a20a9362b10ce
huangyukai <wekohyk@gmail.com>
2024-03-27 17:01:06 +0800

    install lodash-es & use omit to remove props that are not needed

--- src/components/Icon/Icon.vue
+++ src/components/Icon/Icon.vue
@@ -1,12 +1,17 @@
 <template>
   <i class="weko-icon" :class="{ [`weko-icon-${type}`]: type }">
-    <font-awesome-icon v-bind="props"></font-awesome-icon>
+    <font-awesome-icon v-bind="filteredProps"></font-awesome-icon>
   </i>
 </template>
 
 <script setup lang="js">
+import { computed } from 'vue'
 import propsObj from './props.js'
+import { omit } from 'lodash-es'
 const props = defineProps(propsObj)
+
+// use omit to remove props that are not needed
+const filteredProps = computed(() => omit(props, ['type']))
 </script>
 
 <style scoped lang="scss"></style>
