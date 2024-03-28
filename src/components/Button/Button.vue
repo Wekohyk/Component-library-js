ff665dbfa0c4ee54f77329185c52cdde74f9d204
huangyukai <wekohyk@gmail.com>
2024-03-28 11:04:33 +0800

    complete button components

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,22 +1,40 @@
 <template>
   <button
+    ref="_ref"
     class="weko-button"
     :class="{
-      [`weko-button--${type}`]: type
+      [`weko-button--${type}`]: type,
+      [`weko-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled || loading
     }"
+    :disabled="disabled || loading"
   >
-    <slot></slot>
+    <Weko-Icon icon="spinner" spin v-if="loading"></Weko-Icon>
+    <Weko-Icon :icon="icon" v-if="icon"></Weko-Icon>
+    <span v-if="$slots.default">
+      <slot></slot>
+    </span>
   </button>
 </template>
 
 <script setup lang="js">
+import { ref } from 'vue'
 import propsObj from './props'
 
 defineOptions({
   name: 'Weko-Button'
 })
-
 defineProps(propsObj)
+
+// 可以通过wekoButton获取到button元素
+const _ref = ref(null)
+
+defineExpose({
+  ref: _ref
+})
 </script>
 
 <style scoped lang="scss"></style>
--- src/views/Button.vue
+++ src/views/Button.vue
@@ -8,8 +8,83 @@
     <Weko-Button type="warning">警告按钮</Weko-Button>
     <Weko-Button type="danger">危险按钮</Weko-Button>
   </div>
+  <h2 class="row-title">{{ $t('selectSize') }}</h2>
+  <div class="row">
+    <Weko-Button plain>默认按钮</Weko-Button>
+    <Weko-Button type="primary" plain>主要按钮</Weko-Button>
+    <Weko-Button type="info" plain>信息按钮</Weko-Button>
+    <Weko-Button type="success" plain>成功按钮</Weko-Button>
+    <Weko-Button type="warning" plain>警告按钮</Weko-Button>
+    <Weko-Button type="danger" plain>危险按钮</Weko-Button>
+  </div>
+  <h2 class="row-title">圆角按钮</h2>
+  <div class="row">
+    <Weko-Button round>默认按钮</Weko-Button>
+    <Weko-Button type="primary" round>主要按钮</Weko-Button>
+    <Weko-Button type="info" round>信息按钮</Weko-Button>
+    <Weko-Button type="success" plain round>成功按钮</Weko-Button>
+    <Weko-Button type="warning" plain round>警告按钮</Weko-Button>
+    <Weko-Button type="danger" plain round>危险按钮</Weko-Button>
+  </div>
+  <h2 class="row-title">禁用状态</h2>
+  <div class="row">
+    <Weko-Button disabled>默认按钮</Weko-Button>
+    <Weko-Button type="primary" disabled>主要按钮</Weko-Button>
+    <Weko-Button type="info" disabled>信息按钮</Weko-Button>
+    <Weko-Button type="success" disabled>成功按钮</Weko-Button>
+    <Weko-Button type="warning" disabled>警告按钮</Weko-Button>
+    <Weko-Button type="danger" disabled>危险按钮</Weko-Button>
+  </div>
+  <h2 class="row-title">等待状态</h2>
+  <div class="row">
+    <Weko-Button loading>默认按钮</Weko-Button>
+    <Weko-Button type="primary" loading>主要按钮</Weko-Button>
+    <Weko-Button type="info" loading>信息按钮</Weko-Button>
+    <Weko-Button type="success" loading round>成功按钮</Weko-Button>
+    <Weko-Button type="warning" loading round>警告按钮</Weko-Button>
+    <Weko-Button type="danger" loading round>危险按钮</Weko-Button>
+  </div>
+  <h2 class="row-title">图标按钮</h2>
+  <div class="row">
+    <Weko-Button icon="file">默认按钮</Weko-Button>
+    <Weko-Button type="primary" icon="gear">主要按钮</Weko-Button>
+    <Weko-Button type="info" icon="folder" plain>信息按钮</Weko-Button>
+    <Weko-Button type="success" icon="bug" plain>成功按钮</Weko-Button>
+    <Weko-Button type="warning" icon="code" round>警告按钮</Weko-Button>
+    <Weko-Button type="danger" icon="cube" round>危险按钮</Weko-Button>
+  </div>
+  <h2 class="row-title">不同尺寸的按钮</h2>
+  <div class="row">
+    <Weko-Button icon="file" size="small">默认按钮</Weko-Button>
+    <Weko-Button type="primary" icon="gear" size="small">主要按钮</Weko-Button>
+    <Weko-Button type="info" icon="folder" plain>信息按钮</Weko-Button>
+    <Weko-Button type="success" icon="bug" plain>成功按钮</Weko-Button>
+    <Weko-Button type="warning" icon="code" plain size="large">警告按钮</Weko-Button>
+    <Weko-Button type="danger" icon="cube" plain size="large">危险按钮</Weko-Button>
+  </div>
+  <h2 class="row-title">圆形按钮</h2>
+  <div class="row">
+    <Weko-Button type="danger" circle>验</Weko-Button>
+    <Weko-Button type="primary" icon="gear" circle></Weko-Button>
+    <Weko-Button type="info" icon="folder" circle plain></Weko-Button>
+    <Weko-Button type="success" icon="bug" circle plain></Weko-Button>
+    <Weko-Button type="warning" icon="code" circle disabled></Weko-Button>
+    <Weko-Button type="danger" icon="cube" circle disabled></Weko-Button>
+  </div>
+  <h2 class="row-title">按钮事件</h2>
+  <div class="row">
+    <Weko-Button type="primary" @click="clickHandler" ref="btnRef">主要按钮</Weko-Button>
+  </div>
 </template>
 
-<script setup lang="js"></script>
+<script setup lang="js">
+import { ref } from 'vue'
+const btnRef = ref(null)
+
+const clickHandler = () => {
+  window.alert('button click')
+  console.log(btnRef.value.ref)
+}
+</script>
 
 <style scoped lang="scss"></style>
