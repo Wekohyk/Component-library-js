17cfb4566da75cf72b579f63e4bfd09b1c682d73
huangyukai <wekohyk@gmail.com>
2024-04-02 17:53:14 +0800

    Update router history to use hash instead of history

--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,51 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Icon from '../views/Icon.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import Icon from '../views/Icon.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'Icon',
-      component: Icon
+      component: Icon,
     },
     {
       path: '/button',
       name: 'Button',
-      component: () => import('../views/Button.vue')
+      component: () => import('../views/Button.vue'),
     },
     {
       path: '/card',
       name: 'Card',
-      component: () => import('../views/Card.vue')
+      component: () => import('../views/Card.vue'),
     },
     {
       path: '/dialog',
       name: 'Dialog',
-      component: () => import('../views/Dialog.vue')
+      component: () => import('../views/Dialog.vue'),
     },
     {
       path: '/collapse',
       name: 'Collapse',
-      component: () => import('../views/Collapse.vue')
+      component: () => import('../views/Collapse.vue'),
     },
     {
       path: '/pager',
       name: 'Pager',
-      component: () => import('../views/Pager.vue')
+      component: () => import('../views/Pager.vue'),
     },
     {
       path: '/tooltip',
       name: 'Tooltip',
-      component: () => import('../views/Tooltip.vue')
+      component: () => import('../views/Tooltip.vue'),
     },
     {
       path: '/dropdown',
       name: 'Dropdown',
-      component: () => import('../views/Dropdown.vue')
-    }
+      component: () => import('../views/Dropdown.vue'),
+    },
   ],
-  linkActiveClass: 'active'
-})
+  linkActiveClass: 'active',
+});
 
-export default router
+export default router;
